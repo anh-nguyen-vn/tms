f5e076a9253e19679caec4d0a051e518f0998957
Harry - anh.nguyen <anhndn.1989@gmail.com>
2018-01-02 14:13:14 +0700

    Update readme.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tms
+This is demo product for Charter Du lich Vietnam
+
+@Copyright Harry - anhndn.1989@gmail.com
--- src/main/java/com/charter/vietnam/tms/service/SystemUserService.java
+++ src/main/java/com/charter/vietnam/tms/service/SystemUserService.java
@@ -14,7 +14,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,9 +21,7 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.criteria.Predicate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class SystemUserService {
